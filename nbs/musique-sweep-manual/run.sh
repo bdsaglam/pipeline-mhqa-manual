b6cd3f00aa22a964d2b1998aab91d918e746c7dd
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-01-26 01:46:09 +0300

    add more experiments

--- nbs/musique-sweep-manual/run.sh
+++ nbs/musique-sweep-manual/run.sh
@@ -5,37 +5,17 @@ dvc exp run --queue \
     -S dataset.split='train' \
     -S qa.temperature='0.5' \
     -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
-    -S qa.system_prompt='cot/original.txt' \
-    -S qa.few_shot_examples='manual/cot-original.json'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique-sweep' \
-    -S dataset.split='train' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
-    -S qa.system_prompt='cte/format-triplets-few.txt' \
-    -S qa.few_shot_examples='manual/cte.json'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique-sweep' \
-    -S dataset.split='train' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='1' \
-    -S qa.system_prompt='cte/format-triplets-few.txt' \
-    -S qa.few_shot_examples='manual/cte.json'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique-sweep' \
-    -S dataset.split='train' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='cq.txt' \
     -S qa.n_shot='1' \
     -S qa.system_prompt='ccot/format-thought.txt' \
     -S qa.few_shot_examples='manual/ccot.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique-sweep' \
+    -S dataset.split='train' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.system_prompt='cte/format-triplets-few.txt' \
+    -S qa.few_shot_examples='manual/cte.json'
 
