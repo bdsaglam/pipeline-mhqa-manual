9a8e630692bce8df93aacb3fabefc80a240460df
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-01-26 00:35:22 +0300

    add sweep with manual fs examples

---
+++ nbs/musique-sweep-manual/run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique-sweep' \
+    -S dataset.split='train' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.system_prompt='cot/format-thought.txt' \
+    -S qa.few_shot_examples='manual/cot.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique-sweep' \
+    -S dataset.split='train' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='2' \
+    -S qa.system_prompt='cot/format-thought.txt' \
+    -S qa.few_shot_examples='manual/cot.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique-sweep' \
+    -S dataset.split='train' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='1' \
+    -S qa.system_prompt='cot/format-thought.txt' \
+    -S qa.few_shot_examples='manual/cot.json'
+
