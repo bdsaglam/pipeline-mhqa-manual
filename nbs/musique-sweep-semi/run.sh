37ea3eb37dc44eeed5c91c05aa7bcc3da6ac01ef
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-01-26 02:46:59 +0300

    add more experiments

--- nbs/musique-sweep-semi/run.sh
+++ nbs/musique-sweep-semi/run.sh
@@ -16,16 +16,6 @@ dvc exp run --queue \
     -S qa.temperature='0.5' \
     -S qa.user_prompt_template='cq.txt' \
     -S qa.n_shot='1' \
-    -S qa.system_prompt='cot/format-thought.txt' \
-    -S qa.few_shot_examples='semi/cot.json'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique-sweep' \
-    -S dataset.split='train' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='1' \
     -S qa.system_prompt='direct/format-few.txt' \
     -S qa.few_shot_examples='semi/direct.json'
 
@@ -46,26 +36,6 @@ dvc exp run --queue \
     -S qa.temperature='0.5' \
     -S qa.user_prompt_template='cq.txt' \
     -S qa.n_shot='0' \
-    -S qa.system_prompt='direct/format-few.txt' \
-    -S qa.few_shot_examples='semi/direct.json'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique-sweep' \
-    -S dataset.split='train' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='1' \
-    -S qa.system_prompt='cot/original.txt' \
-    -S qa.few_shot_examples='semi/cot-original.json'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique-sweep' \
-    -S dataset.split='train' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
     -S qa.system_prompt='cot/original.txt' \
     -S qa.few_shot_examples='semi/cot-original.json'
 
@@ -95,7 +65,7 @@ dvc exp run --queue \
     -S dataset.split='train' \
     -S qa.temperature='0.5' \
     -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='1' \
-    -S qa.system_prompt='ccot/format-thought.txt' \
-    -S qa.few_shot_examples='semi/ccot.json'
+    -S qa.n_shot='0' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='semi/direct.json'
 
