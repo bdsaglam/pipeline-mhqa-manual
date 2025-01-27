362767ab234109062fef76e4cc1bfe7ba6b259b1
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-01-27 10:22:56 +0300

    reset

---
+++ reset.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Backup
+
+mv dvc.yaml dvc.yaml.bak
+mv params.yaml params.yaml.bak
+mv .dvcignore .dvcignore.bak
+
+mkdir -p tmp/backup/.dvc
+mv .dvc/config tmp/backup/.dvc/config
+mv .dvc/.gitignore tmp/backup/.dvc/.gitignore
+
+rm -rf data/generated
+cp -r data/ tmp/backup/data/
+
+# Destroy
+
+rm -rf .dvc/cache
+rm -rf .dvc/tmp
+
+dvc destroy -f
+
+aws s3 rm s3://bds-dvc/pipeline-mhqa-manual --recursive --profile dvc
+
+# Restore
+
+dvc init
+mv dvc.yaml.bak dvc.yaml
+mv params.yaml.bak params.yaml
+rm .dvcignore
+mv .dvcignore.bak .dvcignore
+
+rm .dvc/config
+mv tmp/backup/.dvc/config .dvc/config
+
+rm .dvc/.gitignore
+mv tmp/backup/.dvc/.gitignore .dvc/.gitignore
+
+rm .dvc/.gitignore
+mv .dvc/.gitignore.bak .dvc/.gitignore
+
+mv tmp/backup/data data
+
+dvc add data/raw
