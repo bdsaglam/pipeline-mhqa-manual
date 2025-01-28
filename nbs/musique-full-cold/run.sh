d93b2500d1b4205bf73886a204ce437db9fd84a2
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-01-28 22:23:45 +0300

    cte triples

--- nbs/musique-full-cold/run.sh
+++ nbs/musique-full-cold/run.sh
@@ -7,10 +7,10 @@ dvc exp run --queue \
     -S qa.model='llama-3-70b-tgi' \
     -S qa.temperature='0.0' \
     -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='3' \
+    -S qa.n_shot='0' \
     -S qa.n_sc='1' \
-    -S qa.system_prompt='direct/format-few.txt' \
-    -S qa.few_shot_examples='semi/direct.json'
+    -S qa.system_prompt='cte/format-triples-few.txt' \
+    -S qa.few_shot_examples='semi/cte-triples.json'
 
 dvc exp run --queue \
     -S run=1 \
@@ -21,42 +21,6 @@ dvc exp run --queue \
     -S qa.user_prompt_template='cq.txt' \
     -S qa.n_shot='3' \
     -S qa.n_sc='1' \
-    -S qa.system_prompt='cte/format-triplets-few.txt' \
-    -S qa.few_shot_examples='semi/cte.json'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.model='llama-3-70b-tgi' \
-    -S qa.temperature='0.0' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='3' \
-    -S qa.n_sc='1' \
-    -S qa.system_prompt='cot/format-thought.txt' \
-    -S qa.few_shot_examples='semi/cot.json'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.model='llama-3-70b-tgi' \
-    -S qa.temperature='0.0' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='3' \
-    -S qa.n_sc='1' \
-    -S qa.system_prompt='ccot/format-thought.txt' \
-    -S qa.few_shot_examples='semi/ccot.json'
-
-dvc exp run --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.model='llama-3-70b-tgi' \
-    -S qa.temperature='0.0' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='3' \
-    -S qa.n_sc='1' \
-    -S qa.system_prompt='cok/adapted.txt' \
-    -S qa.few_shot_examples='semi/cok.json'
+    -S qa.system_prompt='cte/format-triples-few.txt' \
+    -S qa.few_shot_examples='semi/cte-triples.json'
 
