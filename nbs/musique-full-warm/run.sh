9bdf27daaa0f5e3bab59f6f9f95d9f7ec4d9e4f1
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-01-28 21:48:54 +0300

    add more experiments

---
+++ nbs/musique-full-warm/run.sh
@@ -0,0 +1,362 @@
+#!/bin/sh
+
+dvc exp run --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='ccot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/ccot.json'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cte/format-triplets-few.txt' \
+    -S qa.few_shot_examples='semi/cte.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='ccot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/ccot.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cte/format-triplets-few.txt' \
+    -S qa.few_shot_examples='semi/cte.json'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='ccot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/ccot.json'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cte/format-triplets-few.txt' \
+    -S qa.few_shot_examples='semi/cte.json'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cok/adapted.txt' \
+    -S qa.few_shot_examples='semi/cok.json'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/cot.json'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cte/format-triplets-few.txt' \
+    -S qa.few_shot_examples='semi/cte.json'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='ccot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/ccot.json'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='semi/direct.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/cot.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cte/format-triplets-few.txt' \
+    -S qa.few_shot_examples='semi/cte.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='semi/direct.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cok/adapted.txt' \
+    -S qa.few_shot_examples='semi/cok.json'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='semi/direct.json'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cok/adapted.txt' \
+    -S qa.few_shot_examples='semi/cok.json'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/cot.json'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/cot.json'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='semi/direct.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/cot.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='ccot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/ccot.json'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cte/format-triplets-few.txt' \
+    -S qa.few_shot_examples='semi/cte.json'
+
+dvc exp run --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cok/adapted.txt' \
+    -S qa.few_shot_examples='semi/cok.json'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='ccot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/ccot.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cok/adapted.txt' \
+    -S qa.few_shot_examples='semi/cok.json'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='0' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cot/format-thought.txt' \
+    -S qa.few_shot_examples='semi/cot.json'
+
+dvc exp run --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='semi/direct.json'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='semi/direct.json'
+
+dvc exp run --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='llama-3-70b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='3' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='cok/adapted.txt' \
+    -S qa.few_shot_examples='semi/cok.json'
+
