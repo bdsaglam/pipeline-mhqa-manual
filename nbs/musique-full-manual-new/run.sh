ab8775151e51f498aba53982ac8f0f0cc64d6d85
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-02-04 19:19:48 +0300

    add exp results

--- nbs/musique-full-manual-new/run.sh
+++ nbs/musique-full-manual-new/run.sh
@@ -5,9 +5,9 @@ dvc exp run -f --queue \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.split='validation' \
     -S qa.model='deepseek-r1-llama-8b-tgi' \
-    -S qa.temperature='1.0' \
+    -S qa.temperature='0.5' \
     -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
+    -S qa.n_shot='1' \
     -S qa.n_sc='1' \
     -S qa.system_prompt='direct/format-few.txt' \
     -S qa.few_shot_examples='manual/direct.json'
@@ -19,79 +19,7 @@ dvc exp run -f --queue \
     -S qa.model='deepseek-r1-llama-8b-tgi' \
     -S qa.temperature='0.0' \
     -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
-    -S qa.n_sc='1' \
-    -S qa.system_prompt='direct/format-few.txt' \
-    -S qa.few_shot_examples='manual/direct.json'
-
-dvc exp run -f --queue \
-    -S run=3 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.model='deepseek-r1-llama-8b-tgi' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
-    -S qa.n_sc='1' \
-    -S qa.system_prompt='direct/format-few.txt' \
-    -S qa.few_shot_examples='manual/direct.json'
-
-dvc exp run -f --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.model='deepseek-r1-llama-8b-tgi' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
-    -S qa.n_sc='1' \
-    -S qa.system_prompt='direct/format-few.txt' \
-    -S qa.few_shot_examples='manual/direct.json'
-
-dvc exp run -f --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.model='deepseek-r1-llama-8b-tgi' \
-    -S qa.temperature='0.0' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
-    -S qa.n_sc='1' \
-    -S qa.system_prompt='direct/format-few.txt' \
-    -S qa.few_shot_examples='manual/direct.json'
-
-dvc exp run -f --queue \
-    -S run=3 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.model='deepseek-r1-llama-8b-tgi' \
-    -S qa.temperature='1.0' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
-    -S qa.n_sc='1' \
-    -S qa.system_prompt='direct/format-few.txt' \
-    -S qa.few_shot_examples='manual/direct.json'
-
-dvc exp run -f --queue \
-    -S run=3 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.model='deepseek-r1-llama-8b-tgi' \
-    -S qa.temperature='0.0' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
-    -S qa.n_sc='1' \
-    -S qa.system_prompt='direct/format-few.txt' \
-    -S qa.few_shot_examples='manual/direct.json'
-
-dvc exp run -f --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.model='deepseek-r1-llama-8b-tgi' \
-    -S qa.temperature='1.0' \
-    -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
+    -S qa.n_shot='1' \
     -S qa.n_sc='1' \
     -S qa.system_prompt='direct/format-few.txt' \
     -S qa.few_shot_examples='manual/direct.json'
@@ -101,9 +29,81 @@ dvc exp run -f --queue \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.split='validation' \
     -S qa.model='deepseek-r1-llama-8b-tgi' \
+    -S qa.temperature='1.0' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='1' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='manual/direct.json'
+
+dvc exp run -f --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='deepseek-r1-llama-8b-tgi' \
     -S qa.temperature='0.5' \
     -S qa.user_prompt_template='cq.txt' \
-    -S qa.n_shot='0' \
+    -S qa.n_shot='1' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='manual/direct.json'
+
+dvc exp run -f --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='deepseek-r1-llama-8b-tgi' \
+    -S qa.temperature='0.0' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='1' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='manual/direct.json'
+
+dvc exp run -f --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='deepseek-r1-llama-8b-tgi' \
+    -S qa.temperature='1.0' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='1' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='manual/direct.json'
+
+dvc exp run -f --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='deepseek-r1-llama-8b-tgi' \
+    -S qa.temperature='1.0' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='1' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='manual/direct.json'
+
+dvc exp run -f --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='deepseek-r1-llama-8b-tgi' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='1' \
+    -S qa.n_sc='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='manual/direct.json'
+
+dvc exp run -f --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.model='deepseek-r1-llama-8b-tgi' \
+    -S qa.temperature='0.0' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_shot='1' \
     -S qa.n_sc='1' \
     -S qa.system_prompt='direct/format-few.txt' \
     -S qa.few_shot_examples='manual/direct.json'
