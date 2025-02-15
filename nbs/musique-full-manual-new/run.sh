f0fa84a556cb48ea8a0a55a9cd47efd274aeb193
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-02-16 00:41:07 +0300

    setup new exps

--- nbs/musique-full-manual-new/run.sh
+++ nbs/musique-full-manual-new/run.sh
@@ -5,11 +5,11 @@ dvc exp run -f --queue \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.split='validation' \
     -S qa.temperature='0.0' \
-    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.user_prompt_template='cq.txt' \
     -S qa.n_sc='1' \
     -S qa.model='deepseek-r1-llama-70b' \
     -S qa.n_shot='1' \
-    -S qa.system_prompt='empty.txt' \
+    -S qa.system_prompt='direct/format-few.txt' \
     -S qa.few_shot_examples='manual/reasoning.json'
 
 dvc exp run -f --queue \
@@ -29,11 +29,11 @@ dvc exp run -f --queue \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.split='validation' \
     -S qa.temperature='0.0' \
-    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.user_prompt_template='cq.txt' \
     -S qa.n_sc='1' \
     -S qa.model='deepseek-r1-llama-70b' \
     -S qa.n_shot='1' \
-    -S qa.system_prompt='empty.txt' \
+    -S qa.system_prompt='direct/format-few.txt' \
     -S qa.few_shot_examples='manual/reasoning.json'
 
 dvc exp run -f --queue \
@@ -49,7 +49,31 @@ dvc exp run -f --queue \
     -S qa.few_shot_examples='manual/reasoning.json'
 
 dvc exp run -f --queue \
-    -S run=3 \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.temperature='0.0' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_sc='1' \
+    -S qa.model='deepseek-r1-llama-70b' \
+    -S qa.n_shot='0' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run -f --queue \
+    -S run=2 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.temperature='0.0' \
+    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.n_sc='1' \
+    -S qa.model='deepseek-r1-llama-70b' \
+    -S qa.n_shot='1' \
+    -S qa.system_prompt='empty.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run -f --queue \
+    -S run=1 \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.split='validation' \
     -S qa.temperature='0.0' \
@@ -65,10 +89,58 @@ dvc exp run -f --queue \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.split='validation' \
     -S qa.temperature='0.0' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_sc='1' \
+    -S qa.model='deepseek-r1-llama-70b' \
+    -S qa.n_shot='1' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run -f --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.temperature='0.0' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_sc='1' \
+    -S qa.model='deepseek-r1-llama-70b' \
+    -S qa.n_shot='0' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run -f --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.temperature='0.0' \
     -S qa.user_prompt_template='icq-format.txt' \
     -S qa.n_sc='1' \
     -S qa.model='deepseek-r1-llama-70b' \
     -S qa.n_shot='0' \
+    -S qa.system_prompt='empty.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run -f --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.temperature='0.0' \
+    -S qa.user_prompt_template='cq.txt' \
+    -S qa.n_sc='1' \
+    -S qa.model='deepseek-r1-llama-70b' \
+    -S qa.n_shot='0' \
+    -S qa.system_prompt='direct/format-few.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run -f --queue \
+    -S run=3 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.temperature='0.0' \
+    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.n_sc='1' \
+    -S qa.model='deepseek-r1-llama-70b' \
+    -S qa.n_shot='1' \
     -S qa.system_prompt='empty.txt' \
     -S qa.few_shot_examples='manual/reasoning.json'
 
