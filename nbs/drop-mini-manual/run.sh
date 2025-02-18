0dc1ac9f99cc3c539cdd54ae3981e8c4f52e6bd3
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-02-18 19:59:54 +0300

    drop-mini llama-3-8b

--- nbs/drop-mini-manual/run.sh
+++ nbs/drop-mini-manual/run.sh
@@ -4,11 +4,71 @@ dvc exp run --queue \
     -S dataset.path='bdsaglam/drop-musique-mini' \
     -S dataset.split='train' \
     -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='cq.txt' \
+    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.n_sc='1' \
+    -S qa.n_shot='1' \
+    -S run='1' \
+    -S qa.model='deepseek-r1-llama-8b' \
+    -S qa.system_prompt='empty.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/drop-musique-mini' \
+    -S dataset.split='train' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.n_sc='1' \
+    -S qa.n_shot='0' \
+    -S run='2' \
+    -S qa.model='deepseek-r1-llama-8b' \
+    -S qa.system_prompt='empty.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/drop-musique-mini' \
+    -S dataset.split='train' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.n_sc='1' \
+    -S qa.n_shot='1' \
+    -S run='2' \
+    -S qa.model='deepseek-r1-llama-8b' \
+    -S qa.system_prompt='empty.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/drop-musique-mini' \
+    -S dataset.split='train' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='icq-format.txt' \
     -S qa.n_sc='1' \
     -S qa.n_shot='0' \
     -S run='3' \
-    -S qa.model='llama-3-8b' \
-    -S qa.system_prompt='ccot/format-thought.txt' \
-    -S qa.few_shot_examples='manual/ccot.json'
+    -S qa.model='deepseek-r1-llama-8b' \
+    -S qa.system_prompt='empty.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/drop-musique-mini' \
+    -S dataset.split='train' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.n_sc='1' \
+    -S qa.n_shot='0' \
+    -S run='1' \
+    -S qa.model='deepseek-r1-llama-8b' \
+    -S qa.system_prompt='empty.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run --queue \
+    -S dataset.path='bdsaglam/drop-musique-mini' \
+    -S dataset.split='train' \
+    -S qa.temperature='0.5' \
+    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.n_sc='1' \
+    -S qa.n_shot='1' \
+    -S run='3' \
+    -S qa.model='deepseek-r1-llama-8b' \
+    -S qa.system_prompt='empty.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
 
