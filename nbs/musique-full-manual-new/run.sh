de9a1ecd3613596edb08c4b44cb70552563cae04
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-02-18 10:44:27 +0300

    add new exps

--- nbs/musique-full-manual-new/run.sh
+++ nbs/musique-full-manual-new/run.sh
@@ -4,95 +4,11 @@ dvc exp run -f --queue \
     -S run=3 \
     -S dataset.path='bdsaglam/musique' \
     -S dataset.split='validation' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='icq-format.txt' \
-    -S qa.n_sc='1' \
-    -S qa.model='deepseek-r1-llama-70b' \
-    -S qa.n_shot='0' \
-    -S qa.system_prompt='empty.txt' \
-    -S qa.few_shot_examples='manual/reasoning.json'
-
-dvc exp run -f --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='icq-format.txt' \
-    -S qa.n_sc='1' \
-    -S qa.model='deepseek-r1-llama-70b' \
-    -S qa.n_shot='1' \
-    -S qa.system_prompt='empty.txt' \
-    -S qa.few_shot_examples='manual/reasoning.json'
-
-dvc exp run -f --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='icq-format.txt' \
-    -S qa.n_sc='1' \
-    -S qa.model='deepseek-r1-llama-70b' \
-    -S qa.n_shot='0' \
-    -S qa.system_prompt='empty.txt' \
-    -S qa.few_shot_examples='manual/reasoning.json'
-
-dvc exp run -f --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
     -S qa.temperature='0.0' \
     -S qa.user_prompt_template='icq-format.txt' \
     -S qa.n_sc='1' \
     -S qa.model='deepseek-r1-llama-70b' \
     -S qa.n_shot='0' \
-    -S qa.system_prompt='empty.txt' \
-    -S qa.few_shot_examples='manual/reasoning.json'
-
-dvc exp run -f --queue \
-    -S run=3 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.temperature='0.0' \
-    -S qa.user_prompt_template='icq-format.txt' \
-    -S qa.n_sc='1' \
-    -S qa.model='deepseek-r1-llama-70b' \
-    -S qa.n_shot='1' \
-    -S qa.system_prompt='empty.txt' \
-    -S qa.few_shot_examples='manual/reasoning.json'
-
-dvc exp run -f --queue \
-    -S run=3 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.temperature='0.0' \
-    -S qa.user_prompt_template='icq-format.txt' \
-    -S qa.n_sc='1' \
-    -S qa.model='deepseek-r1-llama-70b' \
-    -S qa.n_shot='0' \
-    -S qa.system_prompt='empty.txt' \
-    -S qa.few_shot_examples='manual/reasoning.json'
-
-dvc exp run -f --queue \
-    -S run=1 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.temperature='0.0' \
-    -S qa.user_prompt_template='icq-format.txt' \
-    -S qa.n_sc='1' \
-    -S qa.model='deepseek-r1-llama-70b' \
-    -S qa.n_shot='1' \
-    -S qa.system_prompt='empty.txt' \
-    -S qa.few_shot_examples='manual/reasoning.json'
-
-dvc exp run -f --queue \
-    -S run=2 \
-    -S dataset.path='bdsaglam/musique' \
-    -S dataset.split='validation' \
-    -S qa.temperature='0.5' \
-    -S qa.user_prompt_template='icq-format.txt' \
-    -S qa.n_sc='1' \
-    -S qa.model='deepseek-r1-llama-70b' \
-    -S qa.n_shot='1' \
     -S qa.system_prompt='empty.txt' \
     -S qa.few_shot_examples='manual/reasoning.json'
 
@@ -105,6 +21,30 @@ dvc exp run -f --queue \
     -S qa.n_sc='1' \
     -S qa.model='deepseek-r1-llama-70b' \
     -S qa.n_shot='0' \
+    -S qa.system_prompt='empty.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run -f --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.temperature='0.0' \
+    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.n_sc='1' \
+    -S qa.model='deepseek-r1-llama-70b' \
+    -S qa.n_shot='0' \
+    -S qa.system_prompt='empty.txt' \
+    -S qa.few_shot_examples='manual/reasoning.json'
+
+dvc exp run -f --queue \
+    -S run=1 \
+    -S dataset.path='bdsaglam/musique' \
+    -S dataset.split='validation' \
+    -S qa.temperature='0.0' \
+    -S qa.user_prompt_template='icq-format.txt' \
+    -S qa.n_sc='1' \
+    -S qa.model='deepseek-r1-llama-70b' \
+    -S qa.n_shot='1' \
     -S qa.system_prompt='empty.txt' \
     -S qa.few_shot_examples='manual/reasoning.json'
 
