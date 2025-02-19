c32ca6dbb6474e5546c303462d586427f3672323
Barış Deniz Sağlam <bdsaglam@gmail.com>
2025-02-20 01:39:21 +0300

    self-consistency

--- nbs/drop-mini-manual/run.sh
+++ nbs/drop-mini-manual/run.sh
@@ -3,36 +3,39 @@
 dvc exp run --queue \
     -S dataset.path='bdsaglam/drop-musique-mini' \
     -S dataset.split='train' \
-    -S qa.n_sc='1' \
+    -S qa.n_sc='5' \
     -S qa.n_shot='0' \
     -S run='2' \
     -S qa.model='deepseek-r1-llama-70b' \
     -S qa.temperature='0.5' \
     -S qa.top_p='0.95' \
     -S qa.system_prompt='empty.txt' \
+    -S qa.user_prompt_template='icq-format-drop.txt' \
     -S qa.few_shot_examples='drop/reasoning.json'
 
 dvc exp run --queue \
     -S dataset.path='bdsaglam/drop-musique-mini' \
     -S dataset.split='train' \
-    -S qa.n_sc='1' \
+    -S qa.n_sc='5' \
     -S qa.n_shot='0' \
     -S run='1' \
     -S qa.model='deepseek-r1-llama-70b' \
     -S qa.temperature='0.5' \
     -S qa.top_p='0.95' \
     -S qa.system_prompt='empty.txt' \
+    -S qa.user_prompt_template='icq-format-drop.txt' \
     -S qa.few_shot_examples='drop/reasoning.json'
 
 dvc exp run --queue \
     -S dataset.path='bdsaglam/drop-musique-mini' \
     -S dataset.split='train' \
-    -S qa.n_sc='1' \
+    -S qa.n_sc='5' \
     -S qa.n_shot='0' \
     -S run='3' \
     -S qa.model='deepseek-r1-llama-70b' \
     -S qa.temperature='0.5' \
     -S qa.top_p='0.95' \
     -S qa.system_prompt='empty.txt' \
+    -S qa.user_prompt_template='icq-format-drop.txt' \
     -S qa.few_shot_examples='drop/reasoning.json'
 
